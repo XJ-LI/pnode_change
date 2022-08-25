5fa81e4c907bb82c95af946b11f8d07c5386f9f5
Hong Zhang <hongzhang@anl.gov>
2022-08-25 13:24:49 -0500

    Update the pendulum code

--- pendulum_test_Given_z.py
+++ pendulum_test_Given_z.py
@@ -12,7 +12,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import sys
-sys.path.append('/content/petsc/arch-linux-opt/lib')  # edit path to petsc location
+#sys.path.append('/content/petsc/arch-linux-opt/lib')  # edit path to petsc location
 import copy
 import scipy
 from scipy.optimize import fsolve
@@ -24,9 +24,9 @@ matplotlib.use('Agg')
 
 parser = argparse.ArgumentParser('Chemical Akzo Nobel DAE')
 parser.add_argument('--pnode_method', type=str, choices=['beuler', 'cn'], default='cn')
-parser.add_argument('--data_size', type=int, default=25)
+parser.add_argument('--data_size', type=int, default=5)
 parser.add_argument('--steps_per_data_point', type=int, default=1)
-parser.add_argument('--batch_size', type=int, default=15)
+parser.add_argument('--batch_size', type=int, default=5)
 parser.add_argument('--niters', type=int, default=5000)
 parser.add_argument('--lr', type=float, default=0.01)
 parser.add_argument('--test_freq', type=int, default=100)
@@ -44,11 +44,6 @@ os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('device: ', device)
 
-import petsc4py
-sys.argv = [sys.argv[0]] + unknown
-petsc4py.init(sys.argv)
-from pnode import petsc_adjoint_mod
-    
 # device = 'cpu'
 # data generation
 theta_0=np.pi/2
@@ -61,11 +56,9 @@ y0 = -r0*np.cos(theta_0)
 vx0 = r0*theta_dot0*np.cos(theta_0)
 vy0 = r0*theta_dot0*np.sin(theta_0)
 lbda_0 = (m*r0*theta_dot0**2 +  m*g*np.cos(theta_0))/r0 # equilibrium along the rod's axis
-true_y0 = np.array([x0,y0,vx0,vy0,lbda_0])  
+true_y0 = np.array([x0,y0,vx0,vy0,lbda_0])
 true_y0 = torch.from_numpy(true_y0).double()
-t = torch.linspace(0., 1.0, args.data_size+1, dtype=torch.float64)
-
-
+t = torch.linspace(0., 0.5, args.data_size+1, dtype=torch.float64)
 
 if not args.petsc_ts_adapt:
     unknown.append('-ts_adapt_type')
@@ -74,7 +67,8 @@ if not args.petsc_ts_adapt:
     step_size = t_traj[1] - t_traj[0]
 else:
     step_size = 1e-2
-class Lambda(nn.Module):   
+
+class Lambda(nn.Module):
     def forward(self, t, y):
         f = torch.clone(y)
         f[0] = y[2]
@@ -83,8 +77,15 @@ class Lambda(nn.Module):
         f[3] = -y[1]*y[4] - g
         f[4] = y[4]*(y[0]**2 + y[1]**2) + g*y[1] - (y[2]**2 + y[3]**2)
         return f
+
 M = torch.eye(5); M[-1,-1] = 0.
-ode0 = petsc_adjoint_mod.ODEPetsc()        
+
+import petsc4py
+sys.argv = [sys.argv[0]] + unknown
+petsc4py.init(sys.argv)
+from pnode import petsc_adjoint_mod
+
+ode0 = petsc_adjoint_mod.ODEPetsc()
 ode0.setupTS(true_y0, Lambda(), step_size=step_size, enable_adjoint=False, use_dlpack=False, implicit_form=True, method='cn', M=M)
 true_y = ode0.odeint(true_y0, t)
 
@@ -94,7 +95,6 @@ if not args.double_prec:
 true_y = true_y.to(device)
 t = t.to(device)
 true_y0 = true_y[0]
-
 
 # delete the last dimension? algebraic variable should not enter data
 
@@ -146,13 +146,13 @@ def visualize(t, true_y, pred_y, odefunc, itr, name):
         ax3.set_ylabel(r'$v_x$')
         ax3.plot(t.cpu().numpy(), true_y.cpu().numpy()[:, 2], 'g-', linewidth=lw, **marker_style1)
         ax3.plot(t.cpu().numpy(), pred_y.cpu().numpy()[:, 2], 'b--', linewidth=lw, **marker_style2)
-        
+
         ax4.cla()
         ax4.set_xlabel('t')
         ax4.set_ylabel(r'$v_y$')
         ax4.plot(t.cpu().numpy(), true_y.cpu().numpy()[:, 3], 'g-', linewidth=lw, **marker_style1)
         ax4.plot(t.cpu().numpy(), pred_y.cpu().numpy()[:, 3], 'b--', linewidth=lw, **marker_style2)
-        
+
         ax5.cla()
         ax5.set_xlabel('t')
         ax5.set_ylabel(r'$Lambda$')
@@ -163,8 +163,7 @@ def visualize(t, true_y, pred_y, odefunc, itr, name):
         plt.savefig(os.path.join(args.train_dir, 'png')+'/{:03d}'.format(itr)+name)
         plt.draw()
         plt.pause(0.001)
-        
-        
+
 # network setup
 class ODEFunc(nn.Module):
     def __init__(self):
@@ -223,24 +222,16 @@ class ODEFunc(nn.Module):
         self.nfe += 1
         f = torch.clone(y)
         f[:-1] = self.net(y)
-        f[-1] = self.net2(y)
+        # f[-1] = self.net2(y)
+        f[-1] = y[4]*(y[0]**2 + y[1]**2) + g*y[1] - (y[2]**2 + y[3]**2)
         return f
-
-
-
-
-
-
 
 if __name__ == '__main__':
 
     ii = 0
     if not os.path.exists(args.train_dir):
         os.mkdir(args.train_dir)
-    # set up the mass matrix
-    M = torch.eye(5)
-    M[-1,-1] = 0.
-    
+
     func_PNODE = ODEFunc().to(device)
     ode_PNODE = petsc_adjoint_mod.ODEPetsc()
     ode_PNODE.setupTS(true_y0, func_PNODE, step_size=step_size, method=args.pnode_method, enable_adjoint=True, implicit_form=args.implicit_form, use_dlpack=args.use_dlpack, M=M)
@@ -254,25 +245,25 @@ if __name__ == '__main__':
 
     loss_save = torch.zeros(args.niters)
     deviation_save = torch.zeros(args.niters)
-    
+
     loss_func = nn.MSELoss()
     start_PNODE = time.time()
     for itr in range(curr_iter, args.niters + 1):
-        
+
         batch_t, batch_y = get_batch()
         optimizer_PNODE.zero_grad()
         pred_y_PNODE = ode_PNODE.odeint_adjoint(true_y0, batch_t)
         nfe_f_PNODE = func_PNODE.nfe
         func_PNODE.nfe = 0
-        # loss_PNODE = loss_func(pred_y_PNODE, batch_y)
-        loss_PNODE = torch.mean(torch.abs(pred_y_PNODE - batch_y))
+        loss_PNODE = loss_func(pred_y_PNODE, batch_y)
+        # loss_PNODE = torch.mean(torch.abs(pred_y_PNODE - batch_y))
         loss_PNODE.backward()
         optimizer_PNODE.step()
-        
+
         loss_save[itr-1] = loss_PNODE
-        # deviation_save[itr-1] = torch.mean(torch.abs(func_PNODE.net(pred_y_PNODE)[:,-1])) 
-        deviation_save[itr-1] = torch.mean(torch.abs(func_PNODE.net2(pred_y_PNODE))) 
-        
+        # deviation_save[itr-1] = torch.mean(torch.abs(func_PNODE.net(pred_y_PNODE)[:,-1]))
+        deviation_save[itr-1] = torch.mean(torch.abs(func_PNODE.net2(pred_y_PNODE)))
+
         nfe_b_PNODE = func_PNODE.nfe
         func_PNODE.nfe = 0
 
@@ -282,11 +273,10 @@ if __name__ == '__main__':
                 test_t = t
                 test_y = true_y
                 pred_y_PNODE = ode_test_PNODE.odeint_adjoint(true_y0, test_t)
-                print('check equality constraint, deviation: ', deviation_save[itr-1])              
+                print('check equality constraint, deviation: ', deviation_save[itr-1])
                 loss_PNODE_array= loss_PNODE_array + [loss_PNODE.item()] + [torch.mean(torch.abs(pred_y_PNODE - test_y)).cpu()]
                 print('PNODE: Iter {:05d} | Time {:.6f} | Total Loss {:.6f} | NFE-F {:04d} | NFE-B {:04d}'.format(itr, end_PNODE-start_PNODE, loss_PNODE_array[-1], nfe_f_PNODE, nfe_b_PNODE))
 
-                    
                 if loss_PNODE_array[-1] < best_loss:
                     best_loss = loss_PNODE_array[-1]
                     new_best = True
@@ -305,6 +295,6 @@ if __name__ == '__main__':
                     print('Saved new best results (loss={}) at Iter {}'.format(best_loss,itr))
                 ii += 1
                 start_PNODE = time.time()
-                
+
     np.save('loss_save.npy', loss_save.detach().cpu().numpy())
     np.save('deviation_save.npy', deviation_save.detach().cpu().numpy())
